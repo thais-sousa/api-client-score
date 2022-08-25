47512acc22b396576da9faafcf2b49a5693b372a
Thais Sousa <sousatcm@gmail.com>
2022-08-25 11:10:52 -0300

    Create initial project structure

---
+++ src/main/kotlin/com/client_score/ClientScoreApplication.kt
@@ -0,0 +1,11 @@
+package com.client_score
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientScoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClientScoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/client_score/ClientScoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.client_score
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClientScoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
