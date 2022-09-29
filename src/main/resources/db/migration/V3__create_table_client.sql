4cad4bb4d4e639429ddb271e9f2a2f2ea8138387
thais-sousa <sousatcm@gmail.com>
2022-09-29 11:12:02 -0300

    Adjust files

--- src/main/resources/db/migration/V3__create_table_client.sql
+++ src/main/resources/db/migration/V3__create_table_client.sql
@@ -1,6 +1,7 @@
 CREATE TABLE public.client(
     id UUID NOT NULL,
     person_id UUID NOT NULL,
+    current_score INTEGER NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL,
     CONSTRAINT client_pk PRIMARY KEY (id),
     CONSTRAINT person_id_fk FOREIGN KEY (person_id) REFERENCES person (id)
